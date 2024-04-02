17528f0a56c18ad9327e9f55616aa7b4607561e1
Wizqqr <baban1979molotov@gmail.com>
2024-04-02 15:55:25 +0600

    start object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function getColor() {
+  const colors = [
+    '#248787', '#7c22ab',
+    '#387e14',  '#1f1da3',
+    '#916b18',  '#04ff00',
+    '#bd23d9',  '#17ebd7'
+  ]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='main_block'>
+      <p style={{color: getColor()}}>Вы кликнули {count} раз(а)</p>
+      <button onClick={() => setCount(count  + 1)}>click me</button>
+    </div>
+  )
+}
+
+
+
+
 
 export default App;
